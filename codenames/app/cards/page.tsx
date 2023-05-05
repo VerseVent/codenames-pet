77fc9cecbb1e604af72ef4fbbeb889f2123ecab7
VerseVent <mike.82pronka.27a@gmail.com>
2023-05-05 18:49:57 +0200

    init project

---
+++ codenames/app/cards/page.tsx
@@ -0,0 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Cards",
+  description:
+    "This is a meta description. Welcome to codenames cards. Happy coding and have a nice day",
+};
+
+export default function Cards() {
+  return (
+    <div>
+      <h1>Test Card</h1>
+    </div>
+  );
+}
